bd542d021a1914154679df2b34e22fc75c7c3143
mikalel <levymikael@gmail.com>
2019-05-30 23:09:50 +0300

    pen size modifier feature added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const canvas = document.getElementById("canvas");
 var Paint = {};
-var color = document.getElementsByClassName("color");
 
-// canvas size
+// canvas size feature
 Paint.heightChange = function () {
   var heightValue = parseInt(document.getElementById("height").value);
   document.getElementById("canvas").style.height = heightValue;
@@ -17,20 +16,25 @@ Paint.widthChange = function () {
 }
 document.getElementById("setWidth").addEventListener('click', Paint.widthChange);
 
+// Pen size modifier
+dotSize = document.getElementsByClassName("brush_sizes");
+for (var i = 0; i < dotSize.length; i++) {
+  dotSize[i].addEventListener("click", (e) => dotSize = e.target.id);
+}
+
 //dot drawing
 Paint.divCreation = function (e) {
   var newDiv = document.createElement("div");
-  if (color.length != 1) {
-    newDiv.style.backgroundColor = color;
-    newDiv.style.position = "absolute";
-    newDiv.style.height = "10px";
-    newDiv.style.width = "10px";
-    newDiv.style.left = e.clientX - canvas.offsetLeft + 'px';
-    newDiv.style.top = e.clientY - canvas.offsetTop + 'px';
-    canvas.appendChild(newDiv);
-  } else {
-    alert("please select a color first")
-  }
+  newDiv.style.backgroundColor = "black";
+  newDiv.style.backgroundColor = color;
+  newDiv.style.position = "absolute";
+  newDiv.style.height = "10px";
+  newDiv.style.width = "10px";
+  newDiv.style.height = dotSize;
+  newDiv.style.width = dotSize;
+  newDiv.style.left = e.clientX - canvas.offsetLeft + 'px';
+  newDiv.style.top = e.clientY - canvas.offsetTop + 'px';
+  canvas.appendChild(newDiv);
 };
 
 Paint.draw = function (e) {
@@ -41,15 +45,16 @@ Paint.draw = function (e) {
 canvas.addEventListener("mousemove", Paint.draw);
 
 // Color selection
+color = document.getElementsByClassName("color");
 for (var i = 0; i < color.length; i++) {
   color[i].addEventListener("click", (e) => color = e.target.id);
 }
- 
-// clear All feature
-Paint.clear = function (){
-var allLines = canvas.getElementsByTagName("div");
-while(allLines.length>0){
-  canvas.removeChild(allLines[0]);
+
+// clear screen feature
+Paint.clear = function () {
+  var allLines = canvas.getElementsByTagName("div");
+  while (allLines.length > 0) {
+    canvas.removeChild(allLines[0]);
+  }
 }
-}
-document.getElementById("clear_All").addEventListener('click', Paint.clear);
+document.getElementById("clear_screen").addEventListener('click', Paint.clear);
